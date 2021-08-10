30dc912a492a95a65cd19af56fff0715ec783322
vgoylo <y_draiv.by@mail.ru>
2021-08-10 17:01:15 +0300

    Fix mts definition

--- run.rb
+++ run.rb
@@ -7,7 +7,7 @@ if phone.match?(/(\+375|80)(29|44|33|25)\d{3}\d{2}\d{2}/)
     puts 'Life'
   end
 
-  if phone.match?(/(\+375|80)(29)\d{3}\d{2}\d{2}/) 
+  if phone.match?(/(\+375|80)(29)(2|4|6|8)\d{6}/) 
     puts 'Mts'
   end
 
