70db6642c94843c8cf7f21099f581fb69fccbd35
vgoylo <y_draiv.by@mail.ru>
2021-08-10 15:45:34 +0300

    Find life number

--- run.rb
+++ run.rb
@@ -2,7 +2,10 @@ puts 'Enter your phone number'
 
 phone = gets.chomp
 
-if phone.match?(/(\+375|80) (29|44|33|25) \d{3}-\d{2}-\d{2}/) 
+if phone.match?(/(\+375|80)(29|44|33|25)\d{3}\d{2}\d{2}/) 
+  if phone.match?(/(\+375|80)(25)\d{3}\d{2}\d{2}/) 
+    puts 'Life'
+  end
 else
   puts 'Please enter correct phone number'
 end
