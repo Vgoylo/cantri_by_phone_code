5f327486e18bb564148d6ee6e14f220c840c37c5
vgoylo <y_draiv.by@mail.ru>
2021-08-10 15:48:17 +0300

    Find velcome && mts

--- run.rb
+++ run.rb
@@ -6,6 +6,15 @@ if phone.match?(/(\+375|80)(29|44|33|25)\d{3}\d{2}\d{2}/)
   if phone.match?(/(\+375|80)(25)\d{3}\d{2}\d{2}/) 
     puts 'Life'
   end
+
+  if phone.match?(/(\+375|80)(29)\d{3}\d{2}\d{2}/) 
+    puts 'Mts'
+  end
+
+  if phone.match?(/(\+375|80)(44)\d{3}\d{2}\d{2}/) 
+    puts 'Velcome'
+  end
+
 else
   puts 'Please enter correct phone number'
 end
