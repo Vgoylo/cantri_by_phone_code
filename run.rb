46f695d22cdf36e4ca8b51183aec151c75818276
vgoylo <y_draiv.by@mail.ru>
2021-08-10 17:08:49 +0300

    Fix mts && life defintion

--- run.rb
+++ run.rb
@@ -7,11 +7,11 @@ if phone.match?(/(\+375|80)(29|44|33|25)\d{3}\d{2}\d{2}/)
     puts 'Life'
   end
 
-  if phone.match?(/(\+375|80)(29)(2|4|6|8)\d{6}/) 
+  if phone.match?(/(\+375|80)(29)(2|4|6|8)\d{6}/) || phone.match?(/(\+375|80)(33)(6|9)\d{6}/) 
     puts 'Mts'
   end
 
-  if phone.match?(/(\+375|80)(44)\d{3}\d{2}\d{2}/) 
+  if phone.match?(/(\+375|80)(44)\d{3}\d{2}\d{2}/) || phone.match?(/(\+375|80)(29)(1|3|5|7|9)\d{6}/) 
     puts 'Velcome'
   end
 
