6f389d90153bf25b004c1f2f118714e34d1656bf
vgoylo <y_draiv.by@mail.ru>
2021-08-12 16:16:11 +0300

    Add tests for mts

--- spec/lib/phone_spec.rb
+++ spec/lib/phone_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 
 RSpec.describe Phone do 
-  let!(:phone) { Phone.new(number) }
-
   describe "#operator" do
-    context 'life' do     
+    context 'life' do  
+      let(:phone) { Phone.new(number) }   
       let(:number) { '+375259038314' }
     
-      it "return life" do
+      it 'return life' do
         expect(phone.operator).to eq 'Life'
+      end
+    end
+
+    context 'mts' do 
+      let(:numbers) do 
+        ['+375296832636', '+375299832636', '+375293832636', '80292832636', '80298832636', '80296832636', '80294832636']
+      end
+     
+      it 'return mts' do
+        numbers.each do|number|
+          phone = Phone.new(number)
+          expect(phone.operator).to eq 'Mts'
+        end
       end
     end
   end
