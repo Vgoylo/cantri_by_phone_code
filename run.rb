e50d16d6b9ebcb19c6444d69b6a59894f46ea96b
vgoylo <y_draiv.by@mail.ru>
2021-08-25 09:33:37 +0300

    Add ability to enter phone if it is invalid

--- run.rb
+++ run.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'lib/phone'
+ require_relative 'lib/phone'
 
-puts 'Enter your phone number'
+ loop do
+  puts 'Enter your phone number'
 
-phone = Phone.new(gets.chomp)
+  phone = Phone.new(gets.chomp)
 
-if phone.valid?
-  puts phone.operator
-  puts "corect phone number #{phone.format}"
-else
-  puts 'Please enter correct phone number'
+  if phone.valid?
+    puts phone.operator
+    puts "corect phone number #{phone.format}"
+  else
+    puts 'Please enter correct phone number'
+  end
+
+  break if phone.valid?
 end
